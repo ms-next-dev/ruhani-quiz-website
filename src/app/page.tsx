38a86693fd80964f25c950fb19a5fcd7c03002ff
Md. Mobashirul Alam Seam <mobashirulalamseam@gmail.com>
2024-01-09 00:34:37 +0600

    Responsive navbar developed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import Banner from "@/components/homePage/banner";
+
 export default function Home() {
     return (
         <div>
-            <h1 className="flex justify-center items-center mt-80 text-8xl">
+            <Banner />
+            <h1 className="flex justify-center items-center my-80 text-8xl">
                 Welcome to Ruhani Quiz
             </h1>
         </div>
