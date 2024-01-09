7c1c09f3c6e14d6d15573981def4726a43f8ab0a
Monir Hossain Rabby <monirhrabby.personal@gmail.com>
2024-01-09 07:45:28 +0600

    refactor: banner import

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,13 @@
-import Banner from "@/components/homePage/banner";
+import dynamic from "next/dynamic";
+const Banner = dynamic(() => import("@/components/homePage/banner"));
 
 export default function Home() {
-    return (
-        <div>
-            <Banner />
-            <h1 className="flex justify-center items-center my-80 text-8xl">
-                Welcome to Ruhani Quiz
-            </h1>
-        </div>
-    );
+  return (
+    <div>
+      <Banner />
+      <h1 className="flex justify-center items-center my-80 text-8xl">
+        Welcome to Ruhani Quiz
+      </h1>
+    </div>
+  );
 }
