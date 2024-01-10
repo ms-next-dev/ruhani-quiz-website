96c98f982857c016b4ffaa045fc606f7197975e9
Monir Hossain Rabby <monirhrabby.personal@gmail.com>
2024-01-10 08:09:44 +0600

    added: comment in middlware

--- src/middleware.ts
+++ src/middleware.ts
@@ -12,17 +12,26 @@ const { auth } = NextAuth(authConfig);
 export default auth((req) => {
   const { nextUrl } = req;
 
-  // CheckL is logged in
+  // Check: is logged in
   const isLoggedIn = !!req.auth;
 
   const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix);
   const isPublicRoutes = publicRoutes.includes(nextUrl.pathname);
   const isAuthRoutes = authRoutes.includes(nextUrl.pathname);
 
+  /**
+   * This is routes for next auth
+   */
+
   if (isApiAuthRoute) {
     return null;
   }
 
+  /**
+   * Checkz: if auth routes likes ["/login", "/sign-up"]
+   * If auth routes and user already logged in then user will redirect to the [DEFAULT_LOGIN_REDIRECT]
+   * if auth routes and user not logged in then user can see the auth routes to sign-up or login
+   */
   if (isAuthRoutes) {
     if (isLoggedIn) {
       return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl));
@@ -30,6 +39,12 @@ export default auth((req) => {
 
     return null;
   }
+
+  /**
+   * Check: if user not loggedin and not the public routes . Means this is protected routes like ["/profile"]
+   * If user try to visit protected routes but not logged in then user will redirect to the login page to complete login process
+   * Otherwise user can see the protected routes
+   */
 
   if (!isLoggedIn && !isPublicRoutes) {
     return Response.redirect(new URL("/login", nextUrl));
