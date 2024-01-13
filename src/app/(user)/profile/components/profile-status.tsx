1b85475c4492c8582deecdc975f88704b5ecd985
Monir Hossain Rabby <monirhrabby.personal@gmail.com>
2024-01-13 18:32:20 +0600

    added: progress bar

--- src/app/(user)/profile/components/profile-status.tsx
+++ src/app/(user)/profile/components/profile-status.tsx
@@ -3,7 +3,7 @@ import { Card, CardContent } from "@/components/ui/card";
 const ProfileStatus = () => {
   return (
     <Card className="rounded-[20px]">
-      <CardContent className="p-6">
+      <CardContent className="p-6 flex items-center justify-between">
         <div>
           <h3 className="font-semibold">Complete your profile</h3>
           <p className="text-[14px] max-w-[500px] text-slate-600">
@@ -11,7 +11,17 @@ const ProfileStatus = () => {
             experience and make the most of our platform's capabilities
           </p>
         </div>
-        <div></div>
+        <div>
+          <div
+            className="progress-bar flex justify-center items-center"
+            role="progressbar"
+            aria-valuenow={75}
+            aria-valuemin={0}
+            aria-valuemax={100}
+          >
+            45%
+          </div>
+        </div>
       </CardContent>
     </Card>
   );
