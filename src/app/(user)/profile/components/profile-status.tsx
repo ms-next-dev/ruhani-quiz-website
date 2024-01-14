1b15b3f38008002e1bf16e85f1df8f2a61d16a4b
Monir Hossain Rabby <monirhrabby.personal@gmail.com>
2024-01-14 17:32:49 +0600

    added: email verification card

--- src/app/(user)/profile/components/profile-status.tsx
+++ src/app/(user)/profile/components/profile-status.tsx
@@ -1,12 +1,26 @@
+import { Button } from "@/components/ui/button";
 import { Card, CardContent } from "@/components/ui/card";
+import ProfileSuggestionCards from "./profile-suggestion-cards";
 
 const ProfileStatus = () => {
+  return (
+    <section className="flex flex-col gap-8">
+      <ProfileCompletionCard />
+      {/* <EmailVerificationCard /> */}
+      <ProfileSuggestionCards />
+    </section>
+  );
+};
+
+export default ProfileStatus;
+
+const ProfileCompletionCard = () => {
   return (
     <Card className="rounded-[20px]">
       <CardContent className="p-6 flex items-center justify-between">
         <div>
           <h3 className="font-semibold">Complete your profile</h3>
-          <p className="text-[14px] max-w-[500px] text-slate-600">
+          <p className="text-[12px] lg:text-[14px] max-w-[500px] text-slate-600">
             Your profile is 45% complete. Complete it now to maximize your
             experience and make the most of our platform's capabilities
           </p>
@@ -15,7 +29,7 @@ const ProfileStatus = () => {
           <div
             className="progress-bar flex justify-center items-center"
             role="progressbar"
-            aria-valuenow={75}
+            aria-valuenow={45}
             aria-valuemin={0}
             aria-valuemax={100}
           >
@@ -27,4 +41,23 @@ const ProfileStatus = () => {
   );
 };
 
-export default ProfileStatus;
+const EmailVerificationCard = () => {
+  return (
+    <Card className="rounded-[20px]">
+      <CardContent className="p-6 flex items-center justify-between">
+        <div>
+          <h3 className="font-semibold">Please verify your email</h3>
+          <p className="text-[12px] lg:text-[14px] max-w-[500px] text-slate-600">
+            Welcome! 🌟 Verify your email to unlock quizzes. Check inbox/spam.
+            Let's start quizzing!
+          </p>
+        </div>
+        <div>
+          <Button variant="primary" className="rounded-[10px]">
+            Send Email
+          </Button>
+        </div>
+      </CardContent>
+    </Card>
+  );
+};
