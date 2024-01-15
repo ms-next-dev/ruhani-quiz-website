9fbb11e8d4128fd21ec17c1da5fdb6bd75ed94fa
Md. Mobashirul Alam Seam <mobashirulalamseam@gmail.com>
2024-01-16 00:45:09 +0600

    Prayer time added at homepage

--- src/components/homePage/home-topics.tsx
+++ src/components/homePage/home-topics.tsx
@@ -1,49 +1,16 @@
+// Local Imports
+import { getSubjects } from "@/data/subjects";
+import { getTopicsBySubjectId } from "@/data/topic";
 import { roboto } from "@/lib/fonts";
 import TopicCard from "../cards/topic-card";
 
-const HomeTopics = () => {
-    const topics = [
-        {
-            id: "1",
-            name: "Kalimah",
-            img: "https://res.cloudinary.com/dn2pqzag1/image/upload/v1704005547/kaqasqx6rmncolcvriuv.jpg",
-        },
-        {
-            id: "2",
-            name: "Namaz",
-            img: "https://res.cloudinary.com/dn2pqzag1/image/upload/v1704005547/kaqasqx6rmncolcvriuv.jpg",
-        },
-        {
-            id: "3",
-            name: "Saom/Fasting",
-            img: "https://res.cloudinary.com/dn2pqzag1/image/upload/v1704005547/kaqasqx6rmncolcvriuv.jpg",
-        },
-        {
-            id: "4",
-            name: "Hajj",
-            img: "https://res.cloudinary.com/dn2pqzag1/image/upload/v1704005547/kaqasqx6rmncolcvriuv.jpg",
-        },
-        {
-            id: "5",
-            name: "Zakat",
-            img: "https://res.cloudinary.com/dn2pqzag1/image/upload/v1704005547/kaqasqx6rmncolcvriuv.jpg",
-        },
-        {
-            id: "6",
-            name: "Donation",
-            img: "https://res.cloudinary.com/dn2pqzag1/image/upload/v1704005547/kaqasqx6rmncolcvriuv.jpg",
-        },
-        {
-            id: "7",
-            name: "Sunnah",
-            img: "https://res.cloudinary.com/dn2pqzag1/image/upload/v1704005547/kaqasqx6rmncolcvriuv.jpg",
-        },
-        {
-            id: "8",
-            name: "Akhirah",
-            img: "https://res.cloudinary.com/dn2pqzag1/image/upload/v1704005547/kaqasqx6rmncolcvriuv.jpg",
-        },
-    ];
+const HomeTopics = async () => {
+    const subjects = await getSubjects();
+    const islam = subjects.find(
+        (subject) => subject.name.toLowerCase() === "islam"
+    );
+
+    const topics = await getTopicsBySubjectId(islam?.id.toString());
 
     return (
         <div>
@@ -54,7 +21,7 @@ const HomeTopics = () => {
                     backgroundSize: "cover",
                 }}
             >
-                <div className="container p-28">
+                <div className="container p-10 md:p-14 lg:p-20">
                     <h2
                         className={`${roboto.className} text-5xl text-[#FF004C] text-center font-bold mb-2`}
                     >
@@ -62,10 +29,11 @@ const HomeTopics = () => {
                     </h2>
                     <div className="w-full lg:w-2/3 xl:w-1/3 mx-auto h-[2px] bg-black/50 mb-5 lg:mb-20 rounded-full"></div>
 
-                    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5 lg:gap-10">
-                        {topics.map((topic) => (
-                            <TopicCard key={topic.id} topic={topic} />
-                        ))}
+                    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5 lg:gap-8">
+                        {topics !== null &&
+                            topics.map((topic) => (
+                                <TopicCard key={topic.id} topic={topic} />
+                            ))}
                     </div>
                 </div>
             </div>
