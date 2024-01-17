d920556a047b1808e7db6847398c430a689bd5a4
Monir Hossain Rabby <monirhrabby.personal@gmail.com>
2024-01-17 09:50:49 +0600

    verification badge added

--- src/app/(user)/profile/components/my-self-card/my-self-card.tsx
+++ src/app/(user)/profile/components/my-self-card/my-self-card.tsx
@@ -1,11 +1,20 @@
 // Packages
 import { User } from "@prisma/client";
+import { Briefcase, CalendarDays, CheckCircle2, MapPin } from "lucide-react";
+import moment from "moment";
 import Image from "next/image";
 import { getPlaiceholder } from "plaiceholder";
 
 // Local Imports
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Card, CardContent, CardHeader } from "@/components/ui/card";
+import {
+  HoverCard,
+  HoverCardContent,
+  HoverCardTrigger,
+} from "@/components/ui/hover-card";
 import { Separator } from "@/components/ui/separator";
+import { cn } from "@/lib/utils";
 import MySelfBody from "./my-self-body";
 
 interface MySelfCardProps {
@@ -35,7 +44,6 @@ const MySelfCard: React.FC<MySelfCardProps> = async ({ user }) => {
   return (
     <Card className="rounded-[20px] relative shadow-md h-auto ">
       <CardHeader className="relative h-[160px] w-full">
-        {/* <div className="absolute top-0 left-0 h-[160px] bg-red-100 w-full rounded-t-[20px]"></div> */}
         <Image
           src={
             user.coverPhoto ||
@@ -55,26 +63,39 @@ const MySelfCard: React.FC<MySelfCardProps> = async ({ user }) => {
           alt="profile"
           width={100}
           height={100}
-          className="z-20 h-[100px] absolute top-[95%] left-1/2 transform -translate-x-1/2 -translate-y-1/2 rounded-full border-2 border-[#FF004C]"
+          className="z-20 h-[100px] absolute top-[95%] left-[80px] transform -translate-x-1/2 -translate-y-1/2 rounded-full border-[8px] border-white"
           placeholder="blur"
           blurDataURL={base64}
         />
       </CardHeader>
-      <CardContent className="mt-14">
-        <h3 className="text-center text-gray-600 font-medium text-[18px]">
-          {user.first_name} {user.last_name}
-        </h3>
-        <div className="flex justify-center mt-4 gap-8">
-          <div className="flex flex-col items-center">
-            <h3 className="font-semibold">1.1k</h3>
-            <span className="text-[14px] text-gray-400">Quizes</span>
+      <CardContent className="">
+        <section className="flex justify-between">
+          <div className="mt-14">
+            <Name user={user} />
+            <p className="text-[14px] font-medium text-gray-500 flex items-center gap-x-2">
+              <Briefcase className="w-3 h-3" />
+              {user.designation}
+            </p>
+            <p className="text-[14px] font-normal text-gray-500 flex items-center gap-x-2">
+              <MapPin className={cn("w-3 h-3")} />
+              {user.district}
+            </p>
           </div>
-          <Separator orientation="vertical" className="w-5" />
-          <div className="flex flex-col items-center">
-            <h3 className="font-semibold">10k</h3>
-            <span className="text-[14px] text-gray-400">Score</span>
-          </div>
-        </div>
+          <section>
+            <div className="flex justify-center mt-4 gap-8">
+              <div className="flex flex-col items-center">
+                <h3 className="font-semibold">1.1k</h3>
+                <span className="text-[14px] text-gray-400">Quizes</span>
+              </div>
+              <Separator orientation="vertical" className="w-5" />
+              <div className="flex flex-col items-center">
+                <h3 className="font-semibold">10k</h3>
+                <span className="text-[14px] text-gray-400">Score</span>
+              </div>
+            </div>
+          </section>
+        </section>
+
         <Separator className="my-4" />
         <MySelfBody user={user} />
       </CardContent>
@@ -83,3 +104,43 @@ const MySelfCard: React.FC<MySelfCardProps> = async ({ user }) => {
 };
 
 export default MySelfCard;
+
+const Name: React.FC<MySelfCardProps> = ({ user }) => {
+  return (
+    <h3 className="text-left text-gray-600 font-medium text-[18px] flex items-center gap-x-2">
+      {user.first_name} {user.last_name}{" "}
+      <HoverCard>
+        <HoverCardTrigger asChild>
+          <CheckCircle2
+            className={cn(
+              "w-5 h-5 cursor-pointer",
+              user.emailVerified ? "text-main" : ""
+            )}
+          />
+        </HoverCardTrigger>
+        <HoverCardContent className="w-80 bg-white">
+          <div className="flex justify-between space-x-4">
+            <Avatar>
+              <AvatarImage src={user.avatar!} />
+              <AvatarFallback>PC</AvatarFallback>
+            </Avatar>
+            <div className="space-y-1">
+              <h4 className="text-sm font-semibold">
+                @{user.first_name?.split(" ").join("_")}
+              </h4>
+              <p className="text-sm">
+                Account is verified by email address – maintained by @ruhani
+              </p>
+              <div className="flex items-center pt-2">
+                <CalendarDays className="mr-2 h-4 w-4 opacity-70" />{" "}
+                <span className="text-xs text-muted-foreground">
+                  Joined {moment(user.emailVerified).format("LL")}
+                </span>
+              </div>
+            </div>
+          </div>
+        </HoverCardContent>
+      </HoverCard>
+    </h3>
+  );
+};
