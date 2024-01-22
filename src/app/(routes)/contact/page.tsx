7f44f21536c734af8f31294069de8197c813aa60
Md. Mobashirul Alam Seam <mobashirulalamseam@gmail.com>
2024-01-22 23:41:27 +0600

    Incomplete contact page

---
+++ src/app/(routes)/contact/page.tsx
@@ -0,0 +1,17 @@
+import PageBanner from "@/components/page-banner";
+
+const ContactPage = () => {
+    return (
+        <div>
+            <div>
+                <PageBanner
+                    bannerImg="/bg/contact-bg.jpg"
+                    title1="Contact Us"
+                    title2="Let us know about any of your queries"
+                />
+            </div>
+        </div>
+    );
+};
+
+export default ContactPage;
